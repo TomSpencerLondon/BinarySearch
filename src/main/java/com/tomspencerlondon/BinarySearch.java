5c543d35161659f17a023c938c963482d31d7e16
Tom Spencer <thomas.spencer@codurance.com>
2022-11-19 19:14:43 +0000

    Refactor to recursive version

--- src/main/java/com/tomspencerlondon/BinarySearch.java
+++ src/main/java/com/tomspencerlondon/BinarySearch.java
@@ -3,22 +3,26 @@ package com.tomspencerlondon;
 public class BinarySearch {
 
   public int findIndexOf(int key, int[] numbers) {
-    int low = 0;
-    int high = numbers.length - 1;
-    while (low <= high) {
+    return binarySearch(key, numbers, 0, numbers.length - 1);
+  }
+
+  private static int binarySearch(int key, int[] numbers, int low, int high) {
+    return binaryHelper(key, numbers, low, high);
+  }
+
+  private static int binaryHelper(int key, int[] numbers, int low, int high) {
+    if (low <= high && low <= numbers.length) {
       int midpoint = (low + high) / 2;
       if (key == numbers[midpoint]) {
         return midpoint;
       }
 
-      if (key > numbers[midpoint]) {
-        low = midpoint + 1;
-      }
-
       if (key < numbers[midpoint]) {
-        high = midpoint - 1;
+        return binaryHelper(key, numbers, low, midpoint - 1);
       }
+      return binaryHelper(key, numbers, midpoint + 1, high);
     }
+
     return -1;
   }
 }
